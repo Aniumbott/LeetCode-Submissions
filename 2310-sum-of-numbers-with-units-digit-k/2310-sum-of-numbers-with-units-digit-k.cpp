e77cbf8e138adfd8e47e262b34edad88e8e3b207
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-03 11:29:12 +0530

    Time: 3 ms (56.00%), Space: 5.9 MB (80.77%) - LeetHub

--- 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
-    int helper(int num, int i, int k){
-        if((i*k)%10 == k) return -1;
-        if((i*k)%10 == num%10)
-        {
-            cout << i << " ";
-            if(num >= i*k) return i;
-            else return -1;
-        } 
-        return helper(num, i+1, k);
-    }
     int minimumNumbers(int num, int k) {
-        if(num == 0) return 0;
-        if(k == num%10) return 1;
-        if(k > num) return -1;
-        return helper(num, 2, k);
+        if(num==0)return 0;
+        for(int i=1;i<=10;i++){
+            if((i*k)%10==num%10 && i*k<=num)return i;
+        }
+        return -1;
     }
 };
