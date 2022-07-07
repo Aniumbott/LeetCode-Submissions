91b3c58b7015f6eb90855762d35358dae6bea447
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-07 12:56:39 +0530

    Time: 53 ms (94.96%), Space: 22 MB (63.03%) - LeetHub

--- 1447-simplified-fractions/1447-simplified-fractions.cpp
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -2,10 +2,14 @@ class Solution {
 public:
     vector<string> simplifiedFractions(int n) {
         vector<string> ans;
+        string c[n+1];
+        
+        for(int i=0; i<=n; i++) c[i] = to_string(i);
+        
         for(int i=1; i<=n-1; i++){
             for(int j=i+1; j<=n; j++){
                 if(__gcd(i,j) == 1)
-                    ans.push_back(to_string(i)+'/'+to_string(j));
+                    ans.push_back(c[i]+'/'+c[j]);
             }
         }
         return ans;
