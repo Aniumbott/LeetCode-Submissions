3366d4f9ac5dff97030e42922f72b107a8257a77
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-07 12:26:24 +0530

    Time: 42 ms (26.49%), Space: 26 MB (21.39%) - LeetHub

---
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    bool helper(int p1, int p2, int p3, string s1, string s2, string s3, vector<vector<int>> &dp){
+        if(p3 == s3.size()) return true;
+        
+        if(p1<s1.size() && p2<s2.size() && dp[p1][p2]!=-1) return dp[p1][p2];
+        
+        if(p1<s1.size() && p2<s2.size() && s3[p3] == s1[p1] && s3[p3] == s2[p2])
+            return dp[p1][p2] = helper(p1+1, p2, p3+1, s1, s2, s3, dp) || helper(p1, p2+1, p3+1, s1, s2, s3, dp);
+        
+        if(s3[p3] == s1[p1])
+            return helper(p1+1, p2, p3+1, s1, s2, s3, dp);
+        if(s3[p3] == s2[p2])
+            return helper(p1, p2+1, p3+1, s1, s2, s3, dp);
+        
+        return false;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s3.size() != s1.size() + s2.size()) return false;
+        
+        int p1=0, p2=0, p3=0;
+        vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), -1));
+        return helper(p1, p2, p3, s1, s2, s3, dp);
+    }
+};
