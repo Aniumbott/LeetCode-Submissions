2976191265e1a22434ee901b5c05e9448057542a
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-08 20:09:23 +0530

    Time: 413 ms (91.30%), Space: 117.3 MB (64.71%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
         unordered_map<string, string> mp;
-        
         string ans = "";
         
         for(int i=0; i<knowledge.size(); i++)
             mp[knowledge[i][0]] = knowledge[i][1];
         
-        for(int i=0; i<s.size(); i++){
-            
-            int j=i;
-            while(j<s.size() && s[j]!='(') j++;
-            
-            // cout << i << "-" << j << endl;
-            
-            ans += s.substr(i, j-i);
-            
-            i=j+1;
-            while(j<s.size() && s[j]!=')') j++;
-            
-            if(i < s.size())
-                ans += (mp.find(s.substr(i, j-i)) != mp.end())?mp[s.substr(i, j-i)]:"?";
-            
-            else break;
-            
-            i=j;
-            
-            // cout << ans <<endl;
+        
+        int i=0, n=s.size();
+        while(i<n){            
+            if(s[i] == '('){
+                i++;
+                string tp="";
+                while(i<n && s[i] != ')'){
+                    tp+=s[i];
+                    i++;
+                }
+                if(mp.find(tp) == mp.end()) ans+="?";
+                else ans += mp[tp];
+            }
+            else ans+=s[i];
+            i++;
         }
         return ans;
     }
