05adbb8dee136e40ebc9bfad6d76126c1e431025
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-11 13:18:18 +0530

    Time: 27 ms (36.24%), Space: 5.9 MB (31.47%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        string s = to_string(x);
-        for(int i=0; i<s.size()/2; i++){
-            if(s[i] != s[s.size()-i-1]) return false;
+        if (x < 0) {
+            return false;
         }
-        return true;
+        if (x < 10) {
+            return true;
+        }
+        if (x % 10 == 0) {
+            return false;
+        }
+        int y = 0;
+        while (x > y) {
+            y = y * 10 + x % 10;
+            x /= 10;
+        }
+        return x == y || (x == y / 10);
     }
 };
