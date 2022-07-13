b206151c3c874fb59cdae9606fe15ad663813a76
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-13 17:41:26 +0530

    Time: 6 ms (37.98%), Space: 5.8 MB (94.90%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long r = x;
-        while (r*r > x) r = (r + x/r) / 2;
-        return r;
+        long long l=1;
+        long long h=x;
+        while(l<=h){
+            long long mid=l+(h-l)/2;
+            if(mid*mid==x)return (int)mid;
+            else if(mid*mid<x){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return (int)h;
     }
 };
