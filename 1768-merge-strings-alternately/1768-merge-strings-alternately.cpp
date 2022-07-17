0614505b8a1053557eac5d514a86c83162b10eeb
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-17 11:13:01 +0530

    Time: 7 ms (13.00%), Space: 7.2 MB (10.21%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,9 +3,13 @@ public:
     string mergeAlternately(string word1, string word2) {
         int n=min(word1.size(), word2.size()), i;
         string s = "";
+        
         for(i=0; i<n; i++){ s = s + word1[i] + word2[i]; }
-        if(n < word1.size()) s+=word1.substr(i, word1.size()-n);
-        if(n < word2.size()) s+=word2.substr(i, word2.size()-n);
+        
+        while(i < word1.size()){ s += word1[i]; i++; }
+        
+        while(i < word2.size()){ s += word2[i]; i++; }
+        
         return s;
     }
 };
