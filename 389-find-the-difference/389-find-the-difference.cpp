3bbbd7b27e9fcdd6161c0f06fc927d9463a5bf7c
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-17 11:27:41 +0530

    Time: 11 ms (9.71%), Space: 6.4 MB (98.78%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        for(int i=0;i<s.size();i++)
-		t[i+1]+=t[i]-s[i];
-        return t[t.size()-1];
+        char res = t[s.size()];
+		for(int i = 0; i < s.size(); i++) res ^= s[i] ^ t[i];
+		return res;
     }
 };
