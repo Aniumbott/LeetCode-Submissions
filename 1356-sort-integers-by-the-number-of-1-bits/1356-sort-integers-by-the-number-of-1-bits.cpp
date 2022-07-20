754240de00a12626f4f9db30b14d8d0df50ba15d
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-21 01:28:49 +0530

    Time: 4 ms (97.22%), Space: 10 MB (89.01%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -4,8 +4,7 @@ public:
         sort(arr.begin(), arr.end(), [](auto &a, auto &b){
             long long pa = __builtin_popcount(a);
             long long pb = __builtin_popcount(b);
-            if(pa == pb) return a<b;
-            return pa < pb;
+            return (pa == pb)?a<b:pa<pb;
         });
         return arr;
     }
