2d30e9c065f64227db39d898772c48918226caae
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-20 19:31:12 +0530

    Time: 23 ms (89.23%), Space: 9.9 MB (63.50%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    vector<vector<int> > ans;
+    vector<int> v;
+    
+    void helper(int i, int n, int k) {
+        while (i <= n) {
+            v[v.size() - k] = i++;
+            if (k > 1)
+                helper(i, n, k - 1);
+            else
+                ans.push_back(v);
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        v.resize(k);
+        helper(1, n, k);
+        return ans;
+    }
+};
