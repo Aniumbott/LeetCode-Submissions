21caa9879738fedb0020cead44e8f218681a3d3b
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-22 00:34:17 +0530

    Time: 6 ms (56.76%), Space: 11.1 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int i=0;
-        while(i < nums.size() && nums[i] != target) i++;
-        return (i == nums.size())?-1:i;
+        int l=0, r=nums.size()-1;
+        while(l<=r){
+            int m = l + (r-l)/2;
+            
+            if (target == nums[m]) return m;
+            
+            if(nums[l] <= nums[m]){
+                if(target >= nums[l] && target < nums[m]) r = m-1;
+                else l = m+1;
+            }
+            else{
+                if(target <= nums[r] && target > nums[m]) l = m+1;
+                else r = m-1;
+            }
+        }
+        return -1;
     }
 };
