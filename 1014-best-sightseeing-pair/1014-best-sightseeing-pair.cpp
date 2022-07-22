8543e882b87ef97e8dc1f2b8f8c7e04ea7fb0dd1
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-22 11:49:34 +0530

    Time: 67 ms (78.14%), Space: 39.6 MB (26.26%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);cout.tie(0);
+        
         int n = values.size(), maxScore = 0;
         int maxLeft = values[0] + 0;
         for(int j = 1; j < n; j++) {
