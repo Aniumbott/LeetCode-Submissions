b5756cdca4fc78c5f398cabe22d9d1d85fe032a7
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-26 00:22:26 +0530

    Time: 8 ms (67.06%), Space: 9.3 MB (7.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int dfs(int i, int j, int n, vector<vector<int>>& t, vector<vector<int>>& mp) {
+    int memoize(int i, int j, int n, vector<vector<int>>& t, vector<vector<int>>& mp) {
         if (i == n) return 0;
         if (mp[i][j] != -1) return mp[i][j];
         
-        int lower_left = t[i][j] + dfs(i + 1, j, n, t, mp);
-        int lower_right = t[i][j] + dfs(i + 1, j + 1, n, t, mp);
+        int lower_left = t[i][j] + memoize(i + 1, j, n, t, mp);
+        int lower_right = t[i][j] + memoize(i + 1, j + 1, n, t, mp);
         mp[i][j] = min(lower_left, lower_right);
         
         return mp[i][j];
     }
     
+    int tabulate(vector<vector<int>> mat){
+        int dp[mat.size()][mat.size()], d, dg, n = mat.size();
+        for(int j=0; j<n; j++) dp[n-1][j] = mat[n-1][j];
+        for(int i = n-2; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                d = mat[i][j] + dp[i+1][j];
+                dg = mat[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(d, dg);
+            }
+        }
+        return dp[0][0];
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> mp(n, vector<int>(n, -1));
-        return dfs(0, 0, n, triangle, mp);
+        // return memoize(0, 0, n, triangle, mp);
+        return tabulate(triangle);
     }
 };
