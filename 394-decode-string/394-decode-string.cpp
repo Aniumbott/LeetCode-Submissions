a64c5a930b6722d2f990fffaa6712fe9c4e44cf4
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-30 10:46:51 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (56.09%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    string helper(string &s, int &i){
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                    
+                i++;
+                string t = helper(s, i);
+                i++;
+                
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        
+        return res;
+    }
+public:
+    string decodeString(string s) {
+        int i=0;
+        return helper(s, i);
+    }
+};
