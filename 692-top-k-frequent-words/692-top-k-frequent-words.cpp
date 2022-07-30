d6a739e82517c132a6a594caac4f04ce10613cc8
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-07-30 08:26:16 +0530

    Time: 16 ms (75.39%), Space: 12.4 MB (95.28%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<pair<string, int>> mp;
+        sort(words.begin(), words.end());
+        
+        // for(auto c:words) cout << c  << " ";
+        // cout <<endl;
+        
+        for(int i=0; i<words.size(); i++){
+            mp.push_back({words[i], 1});
+            int ct=0;
+            string tp = words[i];
+            while(i < words.size() && tp == words[i]){ ct++; i++; }
+            i--;
+            mp[mp.size()-1].second = ct;
+
+        }
+        
+        // for(auto i:mp)cout << i.first << "-" << i.second << " ";
+        
+        sort(mp.begin(), mp.end(), [](const auto &a, const auto &b){
+            if(a.second == b.second) return a.first < b.first;
+            else return a.second > b.second;
+        });
+        
+        vector<string> ans;
+        for(int i=0; i<k; i++) ans.push_back(mp[i].first);
+        return ans;
+    }
+};
