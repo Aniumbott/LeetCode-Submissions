5cefd9992adbd9e0a4745aa058f476be684d42e9
Aniket Rana <76243585+Aniumbott@users.noreply.github.com>
2022-08-03 18:46:40 +0530

    Time: 103 ms (90.70%), Space: 38.8 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,49 +1,12 @@
 class MyCalendar {
-private:
-    class item{
-        public:
-            int start, end;
-            item *left = NULL;
-            item *right = NULL;
-            item(int start, int end){
-                this->start = start;
-                this->end = end;
-            }
-    };
-    
-    item *head;
-public:
-    MyCalendar() {
-        head = new item(0, 0);
-    }
-    
-    
-    bool insert(item *root, int start, int end){
-        if(!root) return false;
-        if(root->start >= end && root->start > start){
-            if(root->left) return insert(root->left, start, end);  
-            else{
-                root->left = new item(start, end);
-                return true;
-            }
-        } 
-        if(root->end < end && root->end <= start){
-            if(root->right) return insert(root->right, start, end);  
-            else{
-                root->right = new item(start, end);
-                return true;
-            }
-        } 
-        return false;
-    }
-    
+    map<int, int> mp;
+public:  
     bool book(int start, int end) {
-        item *cur = head;
-        if(cur->start == 0 && cur->end == 0){
-            cur->start = start;
-            cur->end = end;
+        auto itr = mp.upper_bound(start);
+        if(itr == mp.end() || end<= itr->second) {
+            mp[end]=start;
             return true;
-        }
-        else return insert(cur, start, end);
+        } 
+        else  return false;
     }
 };
